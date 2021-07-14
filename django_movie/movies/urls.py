63135f43898b0a9d2033cb1a30fe45f03864e3ec
Andrey.Speransky <andrey.speransky@gmail.com>
2021-07-14 21:46:51 +0300

    Окончательный рабочий вариант c поиском на JS

--- django_movie/movies/urls.py
+++ django_movie/movies/urls.py
@@ -7,7 +7,7 @@ from .views import *
 urlpatterns = [
     path("", MoviesView.as_view(), name='index'),
     path("filter/", FilterMoviesView.as_view(), name='filter'),
-    # path("json-filter/", JsonFilterMoviesView.as_view(), name='json_filter'),
+    path("json-filter/", JsonFilterMoviesView.as_view(), name='json_filter'),
     path("search/", Search.as_view(), name='search'),
     path("add-rating/", AddStarRating.as_view(), name='add_rating'),
     path("<slug:slug>/", MovieDetailView.as_view(), name="movie_detail"),
