94ea0721017cb5c35079b6e54bf1041bc493c689
Andrey.Speransky <andrey.speransky@gmail.com>
2021-07-16 22:51:10 +0300

    Подготовка к развертыванию проекта 0.1

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT нужен для сбора статики
 
